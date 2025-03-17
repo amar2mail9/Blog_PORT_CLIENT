8a445bbb2de344ccd8d58fdeb54f86650b0aecb2
amar2mail9 <amar2mail9@gmail.com>
2025-03-17 23:49:56 +0530

    Link add in main posts

--- components/Page/BlogLandingPage/MainPost/MainPosts.jsx
+++ components/Page/BlogLandingPage/MainPost/MainPosts.jsx
@@ -10,6 +10,7 @@ import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import { Button } from "@mui/material";
 import { FcCalendar } from "react-icons/fc";
+import Link from "next/link";
 
 const MainPosts = () => {
   const [count, setCount] = useState(6);
@@ -23,74 +24,76 @@ const MainPosts = () => {
           {blogData.slice(0, count).map((post, idx) => {
             return (
               <div key={idx}>
-                <motion.div
-                  className="rounded-lg overflow-hidden hover:shadow-md hover:shadow-gray-700 duration-500 ease-in-out transition-all"
-                  initial={{ opacity: 0, y: 100 }}
-                  whileInView={{ opacity: 1, y: 0 }}
-                  transition={{
-                    duration: 0.2,
-                    ease: "easeOut",
-                    scale: { type: "spring", bounce: 0.5, stiffness: 1 },
-                  }}
-                  viewport={{ once: false }}
-                >
-                  <Card
-                    sx={{ maxWidth: "100%" }}
-                    className="!bg-gray-900 border !border-gray-800 !rounded-lg !text-white my-swiper-slide"
+                <Link href={`/blog/${post.slug}`}>
+                  <motion.div
+                    className="rounded-lg overflow-hidden hover:shadow-md hover:shadow-gray-700 duration-500 ease-in-out transition-all"
+                    initial={{ opacity: 0, y: 100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{
+                      duration: 0.2,
+                      ease: "easeOut",
+                      scale: { type: "spring", bounce: 0.5, stiffness: 1 },
+                    }}
+                    viewport={{ once: false }}
                   >
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="140"
-                        image={post.featuredImage}
-                        alt="green iguana"
-                        className="transition-all ease-in-out duration-1000 hover:brightness-50"
-                      />
+                    <Card
+                      sx={{ maxWidth: "100%" }}
+                      className="!bg-gray-900 border !border-gray-800 !rounded-lg !text-white my-swiper-slide"
+                    >
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          height="140"
+                          image={post.featuredImage}
+                          alt="green iguana"
+                          className="transition-all ease-in-out duration-1000 hover:brightness-50"
+                        />
 
-                      <CardContent className="!rounded-lg">
-                        <div className="flex gap-3 items-center">
-                          <Typography
-                            gutterBottom
-                            variant="button"
-                            component="div"
-                            className="!text-pink-600 text-[13px] py-1 !bg-gray-800 w-fit px-4 !rounded-md !capitalize"
-                          >
-                            {post.category.length > 14
-                              ? `${post.category.slice(0, 14)}...`
-                              : post.category}
-                          </Typography>
-                          <Typography
-                            gutterBottom
-                            variant="button"
-                            component="div"
-                          >
-                            <span className="flex items-center">
-                              <FcCalendar className="w-6 h-6 p-1" />
-                              <span className="!text-[10px] !text-gray-500">
-                                {post.publishDate}
+                        <CardContent className="!rounded-lg">
+                          <div className="flex gap-3 items-center">
+                            <Typography
+                              gutterBottom
+                              variant="button"
+                              component="div"
+                              className="!text-pink-600 text-[13px] py-1 !bg-gray-800 w-fit px-4 !rounded-md !capitalize"
+                            >
+                              {post.category.length > 14
+                                ? `${post.category.slice(0, 14)}...`
+                                : post.category}
+                            </Typography>
+                            <Typography
+                              gutterBottom
+                              variant="button"
+                              component="div"
+                            >
+                              <span className="flex items-center">
+                                <FcCalendar className="w-6 h-6 p-1" />
+                                <span className="!text-[10px] !text-gray-500">
+                                  {post.publishDate}
+                                </span>
                               </span>
-                            </span>
+                            </Typography>
+                          </div>
+                          <Typography
+                            gutterBottom
+                            variant="h2"
+                            component="h1"
+                            className="!text-gray-100 !line-clamp-2 !text-[16px]"
+                          >
+                            {post.title}
                           </Typography>
-                        </div>
-                        <Typography
-                          gutterBottom
-                          variant="h2"
-                          component="h1"
-                          className="!text-gray-100 !line-clamp-2 !text-[16px]"
-                        >
-                          {post.title}
-                        </Typography>
-                        <Typography
-                          variant="p"
-                          sx={{ color: "text.secondary" }}
-                          className="!line-clamp-3 !text-xs !text-gray-600"
-                        >
-                          {post.description}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </motion.div>
+                          <Typography
+                            variant="p"
+                            sx={{ color: "text.secondary" }}
+                            className="!line-clamp-3 !text-xs !text-gray-600"
+                          >
+                            {post.description}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </motion.div>
+                </Link>
               </div>
             );
           })}
