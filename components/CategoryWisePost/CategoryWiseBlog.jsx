957373a7686c401b0d545be6ba428741ae18792a
Amar Kumar <amar2mail9@gmail.com>
2025-06-20 13:42:52 +0530

    Color and search function add 02

--- components/CategoryWisePost/CategoryWiseBlog.jsx
+++ components/CategoryWisePost/CategoryWiseBlog.jsx
@@ -1,15 +1,7 @@
 "use client";
-// import { blogData } from "@/context/BlogData";
-// import { useParams } from "next/navigation";
-
+import React from "react";
 
 const CategoryWiseBlog = () => {
-  // const { category } = useParams();
-
-  // const blogs = blogData.find((blog) => blog.category === category);
-
-  // console.log(blogs);
-
   return <div></div>;
 };
 
